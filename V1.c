3dbb44bd47834108619bb6ba26f7df9fcb2206ff
samikshasawai-svs <samusawai9@gmail.com>
2026-09-09 10:20:35 +0530

    Add V1.c
    with number input and switch case

---
+++ V1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+   int num;
+   printf("enter number:");
+   scanf("%d",&num);
+   
+   switch(num)
+   {
+      case 1:
+      printf("One");
+      break;
+      case 2:
+      printf("Two");
+      break;
+    }
+}
