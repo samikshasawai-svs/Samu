65851328a481c0acb171aad975226fb8194b8e63
samikshasawai-svs <samusawai9@gmail.com>
2026-09-13 13:02:22 +0530

    Add Samu6.c for basic arithmetic operations

---
+++ Samu6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the two integes");
+    scanf("%d%d",&a, &b);
+    
+    printf("Add, %d\n",a+b);
+    printf("sub,%d\n",a-b);
+    printf("mult,%d\n",a*b);
+    printf("div,%d\n",a/b);
+    
+    return 0;
+}   
+    
