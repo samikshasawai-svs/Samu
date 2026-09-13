85daf06c31446ba3017ccb960fa1959170675e53
samikshasawai-svs <samusawai9@gmail.com>
2026-09-13 13:03:30 +0530

    Add Samu7.c with basic number comparison

---
+++ Samu7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int num20;
+   printf("enter number:");
+   scanf("%i",&num20);
+   if( 20>10)
+   {
+   printf("number is max");
+   }
+   else
+   {
+   printf("number is min");
+   }
+}   
